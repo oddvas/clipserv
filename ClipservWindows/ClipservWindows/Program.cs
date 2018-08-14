56cae8a0eaaf92296d933c47eedcf7b8f27da6f9
Oddvar Standal <oddvas@online.no>
2018-08-14 23:43:19 +0200

    Console app running, waiting for cancel key press.

--- ClipservWindows/ClipservWindows/Program.cs
+++ ClipservWindows/ClipservWindows/Program.cs
@@ -2,14 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ClipservWindows
 {
     public class Program
     {
+        static ManualResetEvent _quitEvent = new ManualResetEvent(false);
+
         public static void Main(string[] args)
         {
+            Console.CancelKeyPress += (sender, eArgs) => {
+                _quitEvent.Set();
+                eArgs.Cancel = true;
+            };
+
+
+
+            _quitEvent.WaitOne();
         }
     }
 }
